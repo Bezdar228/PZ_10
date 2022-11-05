5d72dbfc061b4aac8e99639c4098b96f32e3e660
ПК <@DESKTOP-FU9UH67>
2022-11-05 20:41:06 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int index_1 = str.IndexOf("/*");
+            int index_2 = str.LastIndexOf("*/");
+            str = str.Remove(index_1, index_2 - index_1 + 2);
+
+            int index_3 = str.IndexOf("//");
+            str = str.Remove(index_3);
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
